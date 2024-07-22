9a57619bf84c1021e8ae3001a4cac6465ea534e5
darya zata <darya.zata@gmail.com>
2024-07-22 22:13:34 +0200

    Updates

---
+++ component---src-pages-page-2-js-5fd078253e24ce8dca15.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[771],{5283:function(e,t,a){a.r(t);var l=a(6540),n=a(4794),r=a(8772),c=a(2269);t.default=()=>l.createElement(r.A,null,l.createElement(c.A,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-5fd078253e24ce8dca15.js.map
